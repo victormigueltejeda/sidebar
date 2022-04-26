a818dbe20b789beabcd53e6e7340298497a38fbf
victor miguel <victormigueltejeda@gmail.com>
2022-04-25 23:58:46 -0600

    cambios finales

--- asset/index.js
+++ asset/index.js
@@ -4,10 +4,12 @@ const sidebar_brant = document.querySelector(".sidebar_brant");
 const sidebar_categori = document.querySelector(".sidebar_categori");
 const sidebar_link = document.querySelectorAll(".sidebar_link");
 const sidebar = document.querySelector(".sidebar");
+const sidebar_text = document.getElementById("text");
 
 sidebar_menubutton.addEventListener("click", function () {
   sidebar.classList.toggle("active");
-  sidebar_brant.classList.toggle("p-display");
+  sidebar_brant.classList.toggle("img_hide");
+  sidebar_text.classList.toggle("title_hide");
   sidebar_link.forEach((item) => {
     item.classList.toggle("hide__element");
   });
